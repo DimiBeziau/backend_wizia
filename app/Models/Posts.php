84050ad72f4ecdde926005215dce69bbc32812c4
dimitri beziau <dimitri.beziau@gmail.com>
2025-04-26 15:10:08 +0200

    fix

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Posts extends Model
 {
-    protected $fillable = [
-      "datePost",
-      "idUser",
-      "idPieceJointe",
-      "post"
-    ]
+  protected $fillable = [
+    "datePost",
+    "idUser",
+    "idPieceJointe",
+    "post"
+  ];
 }
