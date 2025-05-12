b006465cc172f0b4deb6655edceeae5036457bb5
dimitri beziau <dimitri.beziau@gmail.com>
2025-05-12 18:23:48 +0200

    Tentative

--- react/src/Page/CreateCompte.jsx
+++ react/src/Page/CreateCompte.jsx
@@ -25,7 +25,7 @@ const CreateCompte = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`${process.env.VITE_API_BASE_URL}auth/register`, {
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}auth/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
